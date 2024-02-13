4e0f8c6279fb13f524efbf035b3a69fc4ba78c4f
Britojaison <britojaison123@gmail.com>
2024-02-13 22:41:03 +0530

    setting up get reqs

--- home.js
+++ home.js
@@ -6,15 +6,18 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app=express()
 const port=3000;
-app.use(express.static("public"));
 
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/view/home.html");
+    res.render(__dirname+"/view/login.ejs");
 });
 
+app.get("/home",(req,res)=>{
+    res.render(__dirname+"/view/home.ejs");
+});
 app.listen(port,()=>{
     console.log(`server running on http://localhost:${port}`);
 })
