9522f1fc187f3b440150d12e7eca21c6cbe6397c
Britojaison <britojaison123@gmail.com>
2024-03-06 11:51:06 +0530

    resistration done

--- home.js
+++ home.js
@@ -26,28 +26,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
 
-    const results = await db.query("select  * from user")
+    //const results = await db.query("select  * from user")
     //console.log(results[0])
     const message = {
         content: "",
     };
     res.render("login.ejs", message);
+    console.log("11223343");
 });
 
 app.post("/login", async (req, res) => {
 
+    //console.log(req.body);
     const regno = req.body.loginRegisterNumber;
     const password = req.body.loginPassword;
-    console.log(password);
+    // console.log(password);
 
 
     try {
         const results = await db.query("select * from user where regno=?", [regno]);
-
+        // console.log(results)
         if (results[0].length == 1) {
             const user = results[0][0];
             const storedpassword = user.password;
-            console.log(storedpassword);
+            //console.log(storedpassword);
             if (storedpassword == password) {
                 res.render("home.ejs");
             }
@@ -69,9 +71,44 @@ app.post("/login", async (req, res) => {
     }
 });
 
+app.post("/register", async (req, res) => {
+    const regno = req.body.registerNumber;
+    const password = req.body.password;
+    const mobileNumber = req.body.mobileNumber;
+    const email = req.body.email;
+    const department = req.body.department;
+
+    try {
+
+        const results = await db.query("select * from user where regno=?", [regno]);
+
+        if (results[0].length == 0) {
+            let departmentid = await db.query("select deptid from department where dept_name=?", [department]);
+            departmentid = departmentid[0][0].deptid;
+            // console.log(departmentid);
+            db.query("insert into user values(?,?,?,?,?)", [regno, password, mobileNumber, email, departmentid]);
+            const message = {
+                content: "",
+            }
+            res.render("login.ejs", message);
+        } else {
+            const message = {
+                content: "<h3>Registration Failed! User already exists.</h3>",
+            }
+
+            res.render('login.ejs', message);
+        }
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 app.get("/home", (req, res) => {
     res.render(__dirname + "/view/home.ejs");
 });
 app.listen(port, () => {
     console.log(`server running on http://localhost:${port}`);
 })
+
+// This piece of code is importtant and it belongs to the most amazing person i have ever known that is Nayana <3
