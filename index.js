aea0ad537ce1a49b85c090563bdacd887e19daa7
Britojaison <britojaison123@gmail.com>
2024-03-24 12:08:18 +0530

    debugging

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const db = mysql
   .createPool({
     host: "localhost",
     user: "root",
-    password: "1234",
-    database: "uems",
+    password: "sqlmakri",
+    database: "bmf",
   })
   .promise();
 
